30bdd16568980e3fb009199c621f71619fdd12a3
Seenow Brandaskan <mccolddry@mail.ru>
2021-09-25 13:06:31 +0300

    vuex

--- frontend/src/components/office/Task.vue
+++ frontend/src/components/office/Task.vue
@@ -5,7 +5,7 @@
             v-bind:style="`background: url(${require('@/assets/img/back-link-2.png')}) repeat`">
                 <div id='overflow'>
                     <div class="task-col card"
-                    v-for="worker in worker" :key="worker.id">
+                    v-for="worker in TaskWorker" :key="worker.id">
                         <div class="row">
                             <div class="col-12 text-center name-block">
                                 <div class="name headline h3 color-white">{{worker.name}}</div>
@@ -41,123 +41,15 @@
 </template>
 <script>
 import TaskModal from "@/components/office/TaskModal.vue"
+import { mapGetters, mapActions } from "vuex";
 export default {
+  computed: mapGetters(["TaskWorker"]),
   components:{
       TaskModal
   },
   data(){
       return{
           taskmodal:false,
-          worker:[
-              {
-                  name:'Микульский Никита',
-                  proffesion:'дизайнер',
-                  id:'0',
-                  tasks:[
-                      {
-                          name:'нарисовать картинки',
-                          time:'25.09.2021',
-                          level:'100',
-                          description:'нарисовать картинки на лендинг',
-                          status:'inactive',
-                          id:'0'
-                      },
-                      {
-                          name:'сделать canvas',
-                          time:'26.09.2021',
-                          level:'200',
-                          description:'нарисовать офис на канвасе',
-                          status:'active',
-                          id:'1'
-                      }
-                  ]
-              },
-              {
-                  name:'Цветков Роман',
-                  proffesion:'разработчик',
-                  id:'1',
-                  tasks:[
-                      {
-                          name:'нарисовать картинки',
-                          time:'25.09.2021',
-                          level:'100',
-                          description:'нарисовать картинки на меню и команду',
-                          status:'active',
-                          id:'0'
-                      },
-                      {
-                          name:'шаблонизировать запросы в json',
-                          time:'26.09.2021',
-                          level:'200',
-                          description:'шаблонизировать запросы в json на flask',
-                          status:'active',
-                          id:'1'
-                      }
-                  ]
-              },
-              {
-                  name:'Четвергтаков Иван',
-                  proffesion:'разработчик',
-                  id:'2',
-                  tasks:[
-                      {
-                          name:'создать БД',
-                          time:'25.09.2021',
-                          level:'300',
-                          description:'создать базу данных на PostgreSQL',
-                          status:'inactive',
-                          id:'0'
-                      },
-                      {
-                          name:'написать запросы',
-                          time:'26.09.2021',
-                          level:'500',
-                          description:'Написать запросы на все функции',
-                          status:'active',
-                          id:'1'
-                      },
-                      {
-                          name:'поднять сервер',
-                          time:'26.09.2021',
-                          level:'500',
-                          description:'Поднять сервер и настроить CORS',
-                          status:'active',
-                          id:'2'
-                      },
-                      {
-                          name:'Выспаться',
-                          time:'26.09.2021',
-                          level:'500',
-                          description:'Хорошенько поспать',
-                          status:'active',
-                          id:'3'
-                      }
-                  ]
-              },
-              {
-                  name:'Столбов Михаил',
-                  proffesion:'мигрант',
-                  id:'3',
-                  tasks:[
-                      {
-                          name:'получить паспорт',
-                          time:'10.09.2017',
-                          level:'10',
-                          description:'получить Российское гражданство',
-                          status:'stillactive',
-                          id:'0'
-                      },
-                      {
-                          name:'нарисовать картинок',
-                          time:'26.09.2021',
-                          level:'500',
-                          description:'Картинки!',
-                          status:'active',
-                          id:'1'
-                      }
-                  ]
-              }
-          ],
           bigTask:{
                 name:'',
                 time:'',
@@ -169,19 +61,21 @@ export default {
           }
       }
   },
-  mounted(){
+  async mounted(){
       this.MenuLink();
+      this.getTask();
   },
   methods:{
+      ...mapActions(["getTask"]),
       MenuLink(){
             document.getElementById("link-card-1").classList.remove('active');
             document.getElementById("link-card-2").classList.remove('active');
             document.getElementById("link-card-3").classList.add('active');
-            let width=this.worker.length*340;
+            let width=this.TaskWorker.length*340;
             document.getElementById('overflow').style.width=width+"px";
       },
       openTask(worker_id, task_id){
-          let worker = this.worker.find((item) => item.id === worker_id);
+          let worker = this.TaskWorker.find((item) => item.id === worker_id);
           let task= worker.tasks.find((item) => item.id === task_id);
           this.bigTask=task;
           this.taskmodal=true;
--- frontend/src/components/office/Team.vue
+++ frontend/src/components/office/Team.vue
@@ -2,8 +2,8 @@
     <div id="team">
         <div class="container mt-5">
             <div class="desc-block text-center">
-                <div class="name headline h3">{{worker[i].name}}</div>
-                <div class="proffesion">{{worker[i].proffesion}}</div>
+                <div class="name headline h3">{{TeamWorker[i].name}}</div>
+                <div class="proffesion">{{TeamWorker[i].proffesion}}</div>
             </div>
             <div class="center-block mt-5 row text-center">
                 <div class="col-12 col-sm-6 col-md-4 col-xl-3 order-2 order-md-1 mt-md-0 mt-2">
@@ -12,7 +12,7 @@
                         <hr>
                         <div class="skills">
                             <div class="round-skill card mt-2"
-                            v-for="skills in worker[i].skills" :key="skills.id">
+                            v-for="skills in TeamWorker[i].skills" :key="skills.id">
                                 <img v-bind:src="skills.img" class='img-fluid'>
                             </div>
                         </div>
@@ -20,18 +20,18 @@
                             <span class='fs-18'>Рейтинг:
                                 <span class='color-red'>
                                     <i class="fa fa-star-half" aria-hidden="true"></i></span>
-                                    {{worker[i].rating}}
+                                    {{TeamWorker[i].rating}}
                                 </span>
                         </div>
                     </div>
                 </div>
                 <div class="col-12 col-md-4 col-xl-6 order-1 order-md-2">
                     <div class="image-block text-center">
-                        <img v-bind:src="worker[i].img" class='img-fluid'>
+                        <img v-bind:src="TeamWorker[i].img" class='img-fluid'>
                     </div>
                     <div class="work-block">
-                        <div class="name headline h3 mt-2">{{worker[i].work}}</div>
-                        <div class="proffesion">{{worker[i].ttd}}</div>
+                        <div class="name headline h3 mt-2">{{TeamWorker[i].work}}</div>
+                        <div class="proffesion">{{TeamWorker[i].ttd}}</div>
                     </div>
                 </div>
                 <div class="col-12 col-sm-6 col-md-4 col-xl-3 order-3 order-md-3 mt-md-0 mt-2">
@@ -39,19 +39,19 @@
                         <div class="text-center mt-2 h5">Контактные данные</div>
                         <hr>
                         <div class="telephone mt-2">
-                            телефон: &nbsp; <span class='h5'><a v-bind:href="worker[i].phone">{{worker[i].phone}}</a></span>
+                            телефон: &nbsp; <span class='h5'><a v-bind:href="TeamWorker[i].phone">{{TeamWorker[i].phone}}</a></span>
                         </div>
                         <div class="mail mt-2">
-                            почта: &nbsp;<span class='h5'><a v-bind:href="`mailto:`+worker[i].mail">{{worker[i].mail}}</a></span>
+                            почта: &nbsp;<span class='h5'><a v-bind:href="`mailto:`+TeamWorker[i].mail">{{TeamWorker[i].mail}}</a></span>
                         </div>
                         <div class="social mt-2">
-                            <a v-bind:href="worker[i].tg">
+                            <a v-bind:href="TeamWorker[i].tg">
                                 <img v-bind:src="require(`@/assets/img/telegram.png`)" class='img-fluid'>
                             </a>
-                            <a v-bind:href="worker[i].vk">    
+                            <a v-bind:href="TeamWorker[i].vk">    
                                 <img v-bind:src="require(`@/assets/img/vk.png`)" class='img-fluid'>
                             </a>
-                            <a v-bind:href="worker[i].fb">  
+                            <a v-bind:href="TeamWorker[i].fb">  
                                 <img v-bind:src="require(`@/assets/img/facebook.png`)" class='img-fluid'>
                             </a>
                         </div>
@@ -69,7 +69,7 @@
             <div class="cards-container">
                 <div class="content" id='overflow'>
                     <button class='btn' v-on:click="Switch(worker.position)"
-                        v-for="worker in worker" :key="worker.id">
+                        v-for="worker in TeamWorker" :key="worker.id">
                             <div class="card">
                                 <div class="round-skill">
                                     <img v-bind:src="worker.img">
@@ -85,233 +85,36 @@
     </div>
 </template>
 <script>
+import { mapGetters, mapActions } from "vuex";
 export default {
+    computed: mapGetters(["TeamWorker"]),
     data(){
         return{
             i:0,
-            worker:[{
-                name:'Микульский Никита',
-                proffesion:'дизайнер',
-                img:require(`@/assets/img/team_icon_2.png`),
-                mail:'makaka@gmail.com',
-                phone:'+7(814)-233-22-11',
-                tg:'',
-                vk:'',
-                fb:'',
-                work:'Работа не вовк',
-                ttd:'12:36:56',
-                position:0,
-                rating:'200',
-                skills:[
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        tooltip:'залупа',
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        tooltip:'елда',
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        tooltip:'елда',
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        tooltip:'елда',
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        tooltip:'елда',
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        tooltip:'елда',
-                    }
-                ]
-            },
-            {
-                name:'Микульский Никита',
-                proffesion:'дизайнер',
-                img:require(`@/assets/img/Worker_icon.png`),
-                mail:'makaka@gmail.com',
-                phone:'+7(814)-233-22-11',
-                tg:'',
-                vk:'',
-                fb:'',
-                work:'Работа не вовк',
-                ttd:'12:36:56',
-                position:1,
-                rating:'200',
-                skills:[
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    }
-                ]
-            },
-            {
-                name:'Микульский Никита',
-                proffesion:'дизайнер',
-                img:require(`@/assets/img/Worker_icon.png`),
-                mail:'makaka@gmail.com',
-                phone:'+7(814)-233-22-11',
-                tg:'',
-                vk:'',
-                fb:'',
-                work:'Работа не вовк',
-                ttd:'12:36:56',
-                position:2,
-                rating:'200',
-                skills:[
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    }
-                ]
-            },
-            {
-                name:'Микульский Никита',
-                proffesion:'дизайнер',
-                img:require(`@/assets/img/Worker_icon.png`),
-                mail:'makaka@gmail.com',
-                phone:'+7(814)-233-22-11',
-                tg:'',
-                vk:'',
-                fb:'',
-                work:'Работа не вовк',
-                ttd:'12:36:56',
-                position:3,
-                rating:'200',
-                skills:[
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    }
-                ]
-            },
-            {
-                name:'Микульский Никита',
-                proffesion:'дизайнер',
-                img:require(`@/assets/img/Worker_icon.png`),
-                mail:'makaka@gmail.com',
-                phone:'+7(814)-233-22-11',
-                tg:'',
-                vk:'',
-                fb:'',
-                work:'Работа не сасать',
-                ttd:'12:36:56',
-                position:4,
-                rating:'200',
-                skills:[
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    },
-                    {
-                        img:require('@/assets/img/Office_icon_re.png'),
-                        
-                    }
-                ]
-            }
-            ]
         }
     },
-    mounted(){
+    async mounted(){
         this.MenuLink();
+        this.getTeam();
     },
     methods:{
+        ...mapActions(['getTeam']),
         MenuLink(){
             document.getElementById("link-card-3").classList.remove('active');
             document.getElementById("link-card-1").classList.remove('active');
             document.getElementById("link-card-2").classList.add('active');
-            let width=this.worker.length*280;
+            let width=this.TeamWorker.length*280;
             document.getElementById('overflow').style.width=width+"px";
         },
         Prev(){
             if(this.i==0){
-                this.i=this.worker.length-1;
+                this.i=this.TeamWorker.length-1;
             }else{
                 this.i--
             }
         },
         Next(){
-            if(this.i==this.worker.length-1){
+            if(this.i==this.TeamWorker.length-1){
                 this.i=0;
             }else{
                 this.i++
